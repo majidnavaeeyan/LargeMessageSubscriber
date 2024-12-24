ca4cfc93b3b3b15312fc3d9268fe311a5edafd72
Majid Navaeian <Majid Navaeian@DESKTOP-72A1GU3>
2024-12-23 22:46:57 -0800

    ADD : add method for simulate large insert data to message broker.

--- LargeMessageSubscriber.Presentation/Controllers/PointController.cs
+++ LargeMessageSubscriber.Presentation/Controllers/PointController.cs
@@ -43,5 +43,39 @@ namespace LargeMessageSubscriber.Presentation.Controllers
         return result;
       }
     }
+
+    [HttpGet("SimulateLargeInsert")]
+    public async Task SimulateLargeInsertAsync()
+    {
+      for (var i = 0; i < 10; i++)
+      {
+        var data = MakeDate();
+        await _pointService.EnqueuMessageToMessageBrokerAsync(data);
+      }
+    }
+
+    private DateTime RandomDay()
+    {
+      var gen = new Random();
+      var start = new DateTime(2024, 12, 20);
+      var range = (DateTime.Today - start).Days;
+      return start.AddDays(gen.Next(range));
+    }
+
+    private List<Domain.DTOs.Point> MakeDate()
+    {
+      var myDictionary = new Dictionary<string, Domain.DTOs.Point>();
+
+      for (int i = 0; i < 5000; i++)
+      {
+        var name = string.Join("", Enumerable.Repeat(0, 5).Select(n => (char)new Random().Next(97, 122)));
+        var item = new Domain.DTOs.Point { Name = name, Timestamp = RandomDay(), Value = new Random().Next(1, 100) };
+        myDictionary.TryAdd(name, item);
+      }
+
+      var model = myDictionary.Select(q => q.Value).ToList();
+
+      return model;
+    }
   }
 }
